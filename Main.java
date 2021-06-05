c80d03450107cbb94f8ff196c56c4b0398588c72
juan carlos <juankarlos.0304@gmail.com>
2021-06-04 19:34:40 -0500

    Siguientes cambios

--- Main.java
+++ Main.java
@@ -1,29 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
 import javax.swing.*;
 import java.awt.event.*;
-import java.util.Random;
-
-public class Main extends JFrame {
-	public static void main(String[] args) {
-		int n = 10;
-		int numero = (int) (Math.random() * n) + 1;
-        System.out.println(numero);
-
-        String[] cadena = new String[3];
-        
-        Main ma = new Main();
+import java.util.concurrent.ThreadLocalRandom;
+/**
+ *
+ * @author carlos
+ */
+public class NewMain {
+    
+    public static void main(String[] args) {
+        NewMain ma = new NewMain();
         ma.menu();
-	}
+    }
 
-	public void menu(){
-		String Columna = JOptionPane.showInputDialog(null, "Nombre de la Columna");
+    public void menu(){
+        String Columna = JOptionPane.showInputDialog(null, "Nombre de la Columna");
+        
+        String[] Lista = {
+            "Email", 
+            "Random", 
+            "Password", 
+            "Code", 
+            "Telephone", 
+            "Boolean", 
+            "Date", 
+            "Fusion", 
+            "Entre"
+        };
+        
+        String tipolista = (String) JOptionPane.showInputDialog(null, "Tipo de dato", "Dato", 
+            JOptionPane.DEFAULT_OPTION, null, Lista, Lista[1]);
+        
+         String Configuraciones = JOptionPane.showInputDialog(null, "Configuracion");
 
-		String[] TipoDato = {
-			"Integer", 
-			"Varchar", 
-			"Boolen", 
-			"Date" 
-		};
-		String resp = (String) JOptionPane.showInputDialog(null, "Tipo de dato", "Dato", 
-			JOptionPane.DEFAULT_OPTION, null, TipoDato, TipoDato[1]);
-	}
+        String[] TipoDato = {
+            "Integer", 
+            "Varchar", 
+            "Boolen", 
+            "Date" 
+        };
+        String typedato = (String) JOptionPane.showInputDialog(null, "Tipo de dato", "Dato", 
+            JOptionPane.DEFAULT_OPTION, null, TipoDato, TipoDato[1]);
+        
+        data(Columna,tipolista,Configuraciones,typedato);
+    }
+    
+    public void data(String col, String lista, String config, String dato){
+       System.out.println(col + " " + lista+ " " +config+" "+ dato);
+       System.out.println(col +"="+ convertItoS(generarRandom(config)) ); 
+    }
+    
+    public int generarRandom(String Configuracion){
+        String [] vect = Configuracion.split(",");
+        int numero = ThreadLocalRandom.current().nextInt(convertStoI(vect[0]), convertStoI(vect[1]) + 1);
+        return numero;
+    }
+    
+    public int convertStoI(String numero){
+        int varI = Integer.parseInt(numero);
+        return varI;
+    }
+    
+    public String convertItoS(int numero){
+        return numero+"";
+    }
+    
 }
