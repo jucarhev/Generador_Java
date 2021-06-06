23b9180baf33322f0b6e2c22e2b0d9babb074ec5
jc <juankarlos.0304@gmail.com>
2021-06-06 15:25:29 -0500

    Modificacion mas metodos

--- JGenerador/src/main/java/Consola/Generador.java
+++ JGenerador/src/main/java/Consola/Generador.java
@@ -3,6 +3,8 @@ package Consola;
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.concurrent.ThreadLocalRandom;
+
+import java.util.Arrays;
 /**
  *
  * @author carlos
@@ -12,44 +14,35 @@ public class Generador {
     /**
      * @param args the command line arguments
      */
+    
+    public String[] Vocals = {"A","E","I","O","U"};
+    String[] Consonant = {"B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"};
+    String[] Alphabet = {"A","E","I","O","U","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"};
+    String[] Numbers = {"1","2","3","4","5","6","7","8","9","0"};
+    String[] extensiones = {"@gmail.com","@outlook.com"};
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Generador gen = new Generador();
-        //gen.menu();
-        
+        gen.test();
     }
     
-    public String DataRandomFronArray(String[] array){
-        int numero = ThreadLocalRandom.current().nextInt( 0, array.length );
-        return array[numero];
+    // @Funcion de tetting
+    public void test(){
+        
     }
     
     public void menu(){
         String Columna = JOptionPane.showInputDialog(null, "Nombre de la Columna");
         
-        String[] Lista = {
-            "Email", 
-            "Random", 
-            "Password", 
-            "Code", 
-            "Telephone", 
-            "Boolean", 
-            "Date", 
-            "Fusion", 
-            "Entre"
-        };
+        String[] Lista = {"Email","Random","Password","Code","Telephone","Boolean","Date","Fusion","Entre"};
         
         String tipolista = (String) JOptionPane.showInputDialog(null, "Tipo de dato", "Dato", 
             JOptionPane.DEFAULT_OPTION, null, Lista, Lista[1]);
         
          String Configuraciones = JOptionPane.showInputDialog(null, "Configuracion");
 
-        String[] TipoDato = {
-            "Integer", 
-            "Varchar", 
-            "Boolen", 
-            "Date" 
-        };
+        String[] TipoDato = {"Integer", "Varchar", "Boolen", "Date" };
         String typedato = (String) JOptionPane.showInputDialog(null, "Tipo de dato", "Dato", 
             JOptionPane.DEFAULT_OPTION, null, TipoDato, TipoDato[1]);
         
@@ -58,7 +51,23 @@ public class Generador {
     
     public void data(String col, String lista, String config, String dato){
        System.out.println(col + " " + lista+ " " +config+" "+ dato);
+       
        System.out.println(col +"="+ convertItoS(generarRandom(config)) ); 
+       
+       if(lista =="Email"){
+           generarEmail(10);
+       }
+    }
+    
+    public String generarEmail(int number){
+        String cadena = "";
+        for (int i = 0; i < (number/2); i++) {
+            cadena += DataRandomFromArray(Consonant);
+            cadena += DataRandomFromArray(Vocals);
+        }
+        cadena+=DataRandomFromArray(extensiones);
+        System.out.print(cadena);
+        return cadena;
     }
     
     public int generarRandom(String Configuracion){
@@ -76,9 +85,34 @@ public class Generador {
         return numero+"";
     }
     
-    String[] Vocals = {"A","E","I","O","U"};
-    String[] Consonant = {"B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"};
-    String[] Alphabet = {"A","E","I","O","U","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"};
-    String[] Numbers = {"1","2","3","4","5","6","7","8","9","0"};
+    public String DataRandomFromArray(String[] array){
+        int numero = ThreadLocalRandom.current().nextInt( 0, array.length );
+        return array[numero];
+    }
+    
+    public String generarCode(int cantidad, String tipo){
+        String cadena="";
+        for (int i = 0; i <(cantidad/2); i++) {
+            if(tipo=="Number"){
+                cadena += DataRandomFromArray(Numbers);
+                cadena += DataRandomFromArray(Numbers);
+            }else if(tipo=="Letters"){
+                cadena += DataRandomFromArray(Alphabet)+DataRandomFromArray(Alphabet);
+            }else if(tipo=="Mixto"){
+                String[] array = arrayFusion(Alphabet,Numbers);
+                cadena += DataRandomFromArray(array)+DataRandomFromArray(array);
+            }
+        }
+        
+        return cadena;
+    }
+    
+    public String[] arrayFusion(String[] array1, String[] array2){
+        String[] nuevo = new String[array1.length + array2.length];
+        System.arraycopy(array1, 0, nuevo, 0, array1.length);
+        System.arraycopy(array2, 0, nuevo, array1.length, array2.length);
+        
+        return nuevo;
+    }
     
 }
